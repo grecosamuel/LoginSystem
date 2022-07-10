317920ffbbc2937ee412fca49785ec170b4f096d
Samuel Greco <s.greco@cybeur.eu>
2022-07-10 14:45:36 +0200

    Load .env config

--- app/index.js
+++ app/index.js
@@ -2,3 +2,11 @@
 const express = require("express");
 const passport = require("passport");
 const mongo = require("mongodb");
+const dotenv = require("dotenv");
+
+// Load .env config
+dotenv.config();
+
+
+
+
