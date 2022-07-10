479796751f7e546430998f6c6320cd642bc74b9a
Samuel Greco <s.greco@cybeur.eu>
2022-07-10 17:02:28 +0200

    Update express configuration

--- app/index.js
+++ app/index.js
@@ -17,6 +17,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static("public"))
 
 // Mongoose Connection
 db.connect(process.env.MONGODB_URL);
