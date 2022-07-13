6172642b1e14da89d745964a3b2bd7f778e7a7c0
Samuel Greco <s.greco@cybeur.eu>
2022-07-13 16:37:58 +0200

    Remove unused code

--- app/index.js
+++ app/index.js
@@ -8,9 +8,11 @@ const passportConfig = require("./config/passportConfig");
 const bodyParser = require('body-parser'); // parser middleware
 const jwt = require("jsonwebtoken");
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const { verifyToken } = require("./config/middleware/auth");
+const expressSession = require("express-session");
 
 
 // Load .env config
@@ -25,29 +27,29 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.static("public"))
 app.use(cookieParser());
-
+app.use(expressSession({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: false,
+    resave: false,
+}));
 // Mongoose Connection
 db.connect(process.env.MONGODB_URL);
 
 // Passport Config
 passport.initialize();
 passportConfig(passport);
+app.use(passport.session());
 
 // Routes 
 app.use("/auth", authRoutes);
+app.use("/user/private", userRoutes);
+
 
 app.get("/", (req, res, next) => {
     res.render("home", {
         pageTitle: "Home"
     });
 })
-
-app.get("/user/private", verifyToken, (req, res, next) => {
-    res.render("whoami", {
-        pageTitle: "Whoami",
-        user: res.locals.user_data.user
-    })
-});
 
 // Start Server
 app.listen(PORT, () => {
